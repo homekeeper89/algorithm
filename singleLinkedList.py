2393c7c47030e886bd51006c26167cce34cd750c
Homekeeper <homekeeper89@gmail.com>
2019-05-12 19:50:59 +0900

    git ignore add

--- singleLinkedList.py
+++ singleLinkedList.py
@@ -1,8 +1,8 @@
 # Node 
 class Node:
     def __init__(self, val=None):
-        self.next = None
         self.value = val
+        self.next = None
 
 class SingleLinkedList:
 
@@ -11,27 +11,47 @@ class SingleLinkedList:
         self.tail = self.head
         self.tot_length = 0
     
+    def lastNode(self):
+        temp_node = self.head
+        last_node = ''
+        before_last_node = ''
+        for i in range(self.tot_length +1):
+            if i == self.tot_length - 1:
+                before_last_node = temp_node
+                last_node = temp_node.next
+            temp_node = temp_node.next
+        return before_last_node, last_node
+
     def push(self, node):
         if self.tot_length == 0:
             self.head.next = node
             node.next = self.tail
         else:
-            for i in range(self.tot_length):
-                temp_node = self.head
+            temp_node = self.head
+            for i in range(self.tot_length +1):
                 if temp_node.next.value is None:
                     temp_node.next = node
                     node.next = self.tail
                 temp_node = temp_node.next
         self.tot_length += 1
+    
+    def pop(self):
+        if self.tot_length == 0:
+            return None
+        else:
+            before_last_node, last_node = self.lastNode()
+            before_last_node.next = self.tail
+            self.tot_length -= 1
+        return last_node
 
     def insert(self, position, node):
         pass
 
     def printAll(self):
+        temp_node = self.head
         for i in range(self.tot_length):
-            temp_node = self.head
             if temp_node.next is None:
-                print('over')
+                print('end')
                 break
             print(i, temp_node.next.value)
             temp_node = temp_node.next
--- test/test_first.py
+++ test/test_first.py
@@ -13,5 +13,33 @@ class TestSingle:
         sl.push(Node('apple'))
         sl.push(Node('banana'))
         assert sl.tot_length == 2
-        # sl.printAll()
-        sl.test()
+        sl.printAll()
+
+    def test_push(self):
+        sl = SingleLinkedList()
+        assert sl.tot_length == 0
+        sl.push(Node('apple'))
+        sl.push(Node('banana'))
+        sl.push(Node('orrange'))
+        assert sl.tot_length == 3
+
+    # def test_sh_push(self):
+    #     sh = SList()
+    #     sh.push('apple')
+    #     sh.push('banana')
+    #     sh.push('orange')
+    #     assert sh.size == 3
+    #     sh.printSList()
+    
+    def test_pop(self):
+        sl = SingleLinkedList()
+        sl.push(Node('apple'))
+        sl.push(Node('banana'))
+        sl.push(Node('orrange'))
+        assert sl.pop().value == 'orrange'
+        assert sl.pop().value == 'banana'
+        assert sl.pop().value == 'apple'
+        assert sl.tot_length == 0
+
+    def test_node(self):
+        Node('apple')
